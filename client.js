4a0954f38521a7430f450495e55d36ccf4faed38
sherylmoser <sherylrmoser@gmail.com>
2021-12-13 11:33:37 -0700

    client tracking done

--- client.js
+++ client.js
@@ -1,9 +1,14 @@
 const net = require('net');
 
-const client = net.createConnection({ port: 5000 }, () => {
-    console.log('connected');
-});
 
-client.on('data', (data) => {
-    console.log(data.toString());
+
+const client = net.createConnection({ port: 4000 }, () => {
+    console.log('connected');
+    client.on('data', (data) => {
+        console.log(data.toString().trim())
+    })
+});
+let input = process.stdin;
+input.on('data', (data) => {
+    client.write(`${data.toString().trim()}`)
 })
--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 const net = require('net');
 
-const server = net.createServer((client) => {
-    client.write('Welcome to the chat room')
-    client.on('end', () => {
-        console.log('client disconnected');
-    });
-    client.pipe(client);
-}).listen(5000);
+const clients = [];
+let count = 0;
 
-console.log('listening on port 5000');
+const server = net.createServer((client) => {
+    client.write('Welcome to the chat room');
+    clients.push({ client: client, id: count });
+    console.log(clients);
+    count++;
+    // console.log(count);
+    server.on('connection', (client) => {
+
+    })
+    client.on('data', (data) => {
+        clients.forEach((user) => {
+            if (user.client == client) {
+                // console.log(user.id);
+                console.log(`client${user.id}: ${data}`);
+            }
+        })
+    })
+    client.on('end', () => {
+        console.log('client disconnected', clients.length);
+        // count--;
+        clients.forEach((user) => {
+            if (user.client == client) {
+                let id = user.id
+                clients.splice(id, 1)
+            }
+        })
+        console.log(`new array length is ${clients.length}`)
+    });
+}).listen(4000, () => {
+    console.log('listening on port 4000');
+});
