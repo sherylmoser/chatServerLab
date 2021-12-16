f55d9e0aec23d191e00ff0b1b262ce975274bce4
sherylmoser <sherylrmoser@gmail.com>
2021-12-16 09:56:52 -0700

    client chat working
    still need to fix disconnect message

--- server.js
+++ server.js
@@ -5,31 +5,36 @@ let count = 0;
 
 const server = net.createServer((client) => {
     client.write('Welcome to the chat room');
-    clients.push({ client: client, id: count });
-    console.log(clients);
+    client.id = count;
+    clients.push(client);
+    // console.log(clients);
     count++;
     // console.log(count);
-    server.on('connection', (client) => {
-
-    })
     client.on('data', (data) => {
         clients.forEach((user) => {
-            if (user.client == client) {
+            if (user === client) {
                 // console.log(user.id);
-                console.log(`client${user.id}: ${data}`);
+                console.log(`client${client.id}: ${data}`);
+            }
+            if (user !== client) {
+                user.write(`client${client.id}: ${data}`)
             }
         })
+
     })
     client.on('end', () => {
         console.log('client disconnected', clients.length);
         // count--;
         clients.forEach((user) => {
-            if (user.client == client) {
-                let id = user.id
+            if (user === client) {
+                let id = client.id
                 clients.splice(id, 1)
             }
+            if (user !== client) {
+                user.write(`client${client.id} has left the chat`)
+            }
         })
-        console.log(`new array length is ${clients.length}`)
+        // console.log(`new array length is ${clients.length}`)
     });
 }).listen(4000, () => {
     console.log('listening on port 4000');
