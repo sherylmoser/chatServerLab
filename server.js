d0d0f614b65c0bfaebdf4e9eb5329468f8b7307f
sherylmoser <sherylrmoser@gmail.com>
2021-12-16 10:11:57 -0700

    fixed disconnection bug. finished chat

--- server.js
+++ server.js
@@ -7,34 +7,26 @@ const server = net.createServer((client) => {
     client.write('Welcome to the chat room');
     client.id = count;
     clients.push(client);
-    // console.log(clients);
     count++;
-    // console.log(count);
     client.on('data', (data) => {
-        clients.forEach((user) => {
-            if (user === client) {
-                // console.log(user.id);
+        clients.forEach((currClient) => {
+            if (currClient === client) {
                 console.log(`client${client.id}: ${data}`);
             }
-            if (user !== client) {
-                user.write(`client${client.id}: ${data}`)
+            if (currClient !== client) {
+                currClient.write(`client${client.id}: ${data}`)
             }
         })
 
     })
     client.on('end', () => {
-        console.log('client disconnected', clients.length);
-        // count--;
-        clients.forEach((user) => {
-            if (user === client) {
-                let id = client.id
-                clients.splice(id, 1)
-            }
-            if (user !== client) {
-                user.write(`client${client.id} has left the chat`)
-            }
+        console.log('client disconnected');
+        clients.filter(currClient => {
+            currClient !== client
         })
-        // console.log(`new array length is ${clients.length}`)
+        clients.forEach(currClient => {
+            currClient.write(`client${client.id} has left the chat`)
+        })
     });
 }).listen(4000, () => {
     console.log('listening on port 4000');
